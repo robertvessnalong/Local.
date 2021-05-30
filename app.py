bf5f97248d82a2247bafa7896e46d49e0f7c5b83
Robert Long <pongrobertlong@gmail.com>
2021-05-29 23:02:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,7 @@ import requests
 import os
 
 app = Flask(__name__)
-uri = os.environ.get("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///local')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','postgresql:///local').replace("://", "ql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'helloworld')
 geo_lookup = GeoLookup(os.environ.get('IPSTACK_API_KEY'))
